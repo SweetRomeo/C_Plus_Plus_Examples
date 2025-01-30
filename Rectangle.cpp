540dc77167769b7befecaf7d9c164bbda421b280
berke sertel <berkesertel4@gmail.com>
2025-01-30 21:59:48 +0300

    New Example has been added

---
+++ Rectangle.cpp
@@ -0,0 +1,17 @@
+//
+// Created by berke on 1/30/2025.
+//
+#include "Rectangle.h"
+
+Rectangle::Rectangle(int length, int breadth) {
+    this->length = length;
+    this->breadth = breadth;
+}
+
+int Rectangle::getLength() const {
+    return this->length;
+}
+
+int Rectangle::getBreadth() const {
+
+}
---
+++ Rectangle.h
@@ -0,0 +1,18 @@
+//
+// Created by berke on 1/30/2025.
+//
+
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+class Rectangle {
+    int length;
+    int breadth;
+public:
+    Rectangle(int length, int breadth);
+    int getLength() const;
+    void setLength(int length);
+    int getBreadth() const;
+    void setBreadth(int breadth);
+};
+
+#endif //RECTANGLE_H

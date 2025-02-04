1532e19a4f20d6b1fe41013446607ae4da4a13bd
berke sertel <berkesertel4@gmail.com>
2025-02-04 23:44:05 +0300

    isArmstrong function has been added

--- NumberUtil.cpp
+++ NumberUtil.cpp
@@ -4,7 +4,18 @@
 
 #include "NumberUtil.h"
 
-int calculateFactorial(const int number) {
+int NumberUtil::signum(int val)
+{
+    if (val < 0)
+        return -1;
+    if (val == 0)
+        return 0;
+    return 1;
+}
+
+
+int NumberUtil::calculateFactorial(const int number)
+{
     return number <= 1 ? 1 : number * calculateFactorial(number - 1);
 }
 
@@ -17,6 +28,15 @@ int NumberUtil::getDigitsCount(int val) {
     return count;
 }
 
+int NumberUtil::getDigitsSum(int val) {
+    int sum = 0;
+    while (val) {
+        sum += val % 10;
+        val /= 10;
+    }
+    return sum;
+}
+
 int NumberUtil::power(const int base, const int exp) {
     int result = 1;
     for (int i = 0; i < exp; i++) {
@@ -24,6 +44,18 @@ int NumberUtil::power(const int base, const int exp) {
     }
     return result;
 }
+
+bool NumberUtil::isArmstrong(const int val) {
+    int result = 0;
+    int count = getDigitsCount(val);
+    int temp = val;
+    while (temp) {
+        result += power(count, temp % 10);
+        temp /= 10;
+    }
+    return result == val;
+}
+
 
 bool NumberUtil::isPowerOfTwo(const int val) {
     return (val & (val - 1)) == 0 && val != 0;
@@ -67,12 +99,13 @@ bool NumberUtil::isPrimeX(int val) {
     return true;
 }
 
-double getENumber() {
+double NumberUtil::getENumber() {
     double e = 1.0;
     for (int i = 0; i < 13; ++i) {
         e += static_cast<double>(1) / calculateFactorial(i);
     }
     return e;
 }
+
 
 
--- NumberUtil.h
+++ NumberUtil.h
@@ -6,8 +6,12 @@
 #define NUMBERUTIL_H
 class NumberUtil {
 public:
+    static int signum(int val) ;
+    static int calculateFactorial(int val);
     static int getDigitsCount(int val);
+    static int getDigitsSum(int val);
     static int power(int base, int exp);
+    static bool isArmstrong(int val);
     static bool isPowerOfTwo(int val);
     static bool isPrime(int val);
     static bool isPrimeX(int val);

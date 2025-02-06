69f6358f53e7cac23f6fb8962a1228be5f25131b
berke sertel <berkesertel4@gmail.com>
2025-02-06 21:19:47 +0300

    New member functions has been added to the NumberUtil class

--- NumberUtil.cpp
+++ NumberUtil.cpp
@@ -109,7 +109,7 @@ double NumberUtil::getENumber() {
     return e;
 }
 
-void NumberUtil::printDiamond(int n) {
+void NumberUtil::printDiamond(const int n) {
     bool directionFlag = true;
     int index = 0;
     while (true) {
@@ -132,7 +132,99 @@ void NumberUtil::printDiamond(int n) {
             break;
         }
         std::cout << "\n";
-
     }
+}
 
+void NumberUtil::printFactors(int val) {
+    int factor = 2;
+    while (val != 1) {
+        if (val % factor == 0) {
+            std::cout << factor << " ";
+            val /= factor;
+        }
+        else {
+            factor++;
+        }
+    }
+}
+
+int NumberUtil::sumFactors(const int val) {
+    int sum = 0;
+    int factor = 1;
+    while (factor <= val / 2) {
+        if (val % factor == 0) {
+            sum += factor;
+        }
+        factor++;
+    }
+    return sum;
+}
+
+bool NumberUtil::isPerfect(const int val) {
+    return sumFactors(val) == val;
+}
+
+int NumberUtil::reverse(int val) {
+    int reversed = 0;
+    while (val != 0) {
+        reversed = reversed * 10 + val % 10;
+        val = val / 10;
+    }
+    return reversed;
+}
+
+bool NumberUtil::isValid(int val) {
+    int a = getHundreds(val);
+    int b = getTens(val);
+    int c = getOnes(val);
+    const int rev = reverse(b);
+    return rev > val && isPrime(rev) && isPrime(val) && isPrime(10 * a + b) && isPrime(10 * b + c) &&
+        isPrime(10 * c + b) && isPrime(10 * b + a);
+}
+
+int NumberUtil::getOnes(const int val) {
+    return val % 10;
+}
+
+int NumberUtil::getTens(const int val) {
+    return val % 100 / 10;
+}
+
+int NumberUtil::getHundreds(const int val) {
+    return val / 100;
+}
+
+int NumberUtil::calculateDigitalRoot(int val) {
+    while (val / 10 != 0) {
+        val = getDigitsSum(val);
+    }
+    return val;
+}
+
+bool NumberUtil::isFactorian(const int val) {
+    int result = 0;
+    int temp = val;
+    do {
+        result += calculateFactorial(temp % 10);
+        temp /= 10;
+    } while (temp);
+    return result == val;
+}
+
+bool NumberUtil::isSuperPrime(int val) {
+    int index = 0;
+    for (int i = 2; i <= val; ++i) {
+        if (isPrime(i)) {
+            ++index;
+        }
+    }
+    return isPrime(index) && isPrime(val);
+}
+
+void NumberUtil::isSuperPrimeTest() {
+    for (int i = 2; i <= 1000; ++i) {
+        if (isSuperPrime(i)) {
+            std::cout << i << std::endl;
+        }
+    }
 }
